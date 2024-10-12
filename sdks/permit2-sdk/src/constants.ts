c7c61b814c576799dc463e71b569892cd553fb32
RitzyDev <RitzyDevBox@proton.me>
2024-10-12 17:01:16 -0400

    adding permit 2 constants

--- sdks/permit2-sdk/src/constants.ts
+++ sdks/permit2-sdk/src/constants.ts
@@ -2,11 +2,26 @@ import { BigNumber } from '@ethersproject/bignumber'
 
 // @deprecated please use permit2Address(chainId: number)
 export const PERMIT2_ADDRESS = '0x000000000022D473030F116dDEE9F6B43aC78BA3'
+export const BTT_PERMIT2_ADDRESS = '0x41846bd0E41dD42D80d00E99212040C871F40CB0'
+export const FTM_PERMIT2_ADDRESS = '0x838c010F57889195C88f2097488e49B7ad04EcB1'
+export const ZEN_PERMIT2_ADDRESS = '0xd119eF12fdC22756c8Ce4F03E463607742e3A59f'
+export const BERA_TESTNET_PERMIT2_ADDRESS = '0x6AB0CA9c94FDE313a3A1d34A8247ae6065Bd2E73'
+export const SONIC_TESTNET_PERMIT2_ADDRESS = '0xc67ED70B32D14EcDdb7d279b9A8D50d00aBD171e'
 
 export function permit2Address(chainId?: number): string {
   switch (chainId) {
     case 324:
       return '0x0000000000225e31D15943971F47aD3022F714Fa'
+    case 199: // btt
+      return BTT_PERMIT2_ADDRESS
+    case 7332:
+      return ZEN_PERMIT2_ADDRESS
+    case 250:
+      return FTM_PERMIT2_ADDRESS
+    case 80084:
+      return BERA_TESTNET_PERMIT2_ADDRESS
+    case 64165:
+      return SONIC_TESTNET_PERMIT2_ADDRESS
     default:
       return PERMIT2_ADDRESS
   }

5d71d251e3c95265ada55e83cc62b582c0de8881
allvme <90796822+allvme@users.noreply.github.com>
2024-11-02 10:50:38 +0100

    Add wbtc address and permit2 address

--- sdks/permit2-sdk/src/constants.ts
+++ sdks/permit2-sdk/src/constants.ts
@@ -7,6 +7,7 @@ export const FTM_PERMIT2_ADDRESS = '0x838c010F57889195C88f2097488e49B7ad04EcB1'
 export const ZEN_PERMIT2_ADDRESS = '0xd119eF12fdC22756c8Ce4F03E463607742e3A59f'
 export const BERA_TESTNET_PERMIT2_ADDRESS = '0x6AB0CA9c94FDE313a3A1d34A8247ae6065Bd2E73'
 export const SONIC_TESTNET_PERMIT2_ADDRESS = '0xc67ED70B32D14EcDdb7d279b9A8D50d00aBD171e'
+export const GOAT_TESTNET_PERMIT2_ADDRESS = '0x7928a2c48754501f3a8064765ECaE541daE5c3E6'
 
 export function permit2Address(chainId?: number): string {
   switch (chainId) {
@@ -22,6 +23,8 @@ export function permit2Address(chainId?: number): string {
       return BERA_TESTNET_PERMIT2_ADDRESS
     case 64165:
       return SONIC_TESTNET_PERMIT2_ADDRESS
+    case 48815:
+      return GOAT_TESTNET_PERMIT2_ADDRESS
     default:
       return PERMIT2_ADDRESS
   }
